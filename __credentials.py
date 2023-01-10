942ff1289f9f89e7001c5b54c09ee5c963cf54c1
Shane Dolan <58960342+shanedoolane@users.noreply.github.com>
2023-01-10 00:26:41 -0800

    Initial commit

---
+++ __credentials.py
@@ -0,0 +1,7 @@
+device_id = ''  # https://github.com/mmohades/Venmo#usage, maybe optional
+access_token = ''  # https://github.com/mmohades/Venmo#usage
+to_email = ''  # address that you want notifications sent to
+from_email = ''  # use gmail
+email_password = ''  # https://myaccount.google.com/apppasswords
+funding_source_id = ''  # https://github.com/mmohades/Venmo#payment-methods
+target_user_id = ''  # https://github.com/mmohades/Venmo#usage
